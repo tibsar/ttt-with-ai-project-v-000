5276bbff6a8fae6c64a96b9d36bd57faf9b420f8
Sara Tibbetts <sxt4866@rit.edu>
2018-04-13 01:38:29 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -55,7 +55,7 @@ module Players
       ##  - determine all spots where opponent is and remove all combos with that position
 
       # Create an array of positions where opponent is located
-      opponent_positions = board.cells.each_index.select{|i| board.taken?(i + 1) && board.position(i + 1) != self.token }
+      opponent_positions = board.cells.each_index.select{|i| board.taken?(i + 1) && board.position(i + 1) != self.token }.map{ |index| index + 1}
     end
 
     ### RETURNS A CURRENTLY EMPTY SPOT IN A WIN COMBO WITH TWO OF THE THREE
